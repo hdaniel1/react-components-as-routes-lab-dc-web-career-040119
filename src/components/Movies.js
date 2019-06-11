af5fec1b61818bafa34d0f8fe8ff95dbf87efe75
Daniel Hallada <hallada.daniel@gmail.com>
2019-06-11 10:26:10 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page </h1>
+      {movies.map(movie => {
+      return(<div>
+        <b>{movie.title}</b><br></br>
+        {movie.time} minutes
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>)
+    })}
     </div>
   );
 };
